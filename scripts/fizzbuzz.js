8982bedc14776bb41380ab29251f3593d0b5fd88
Naoise Gaffney <gaff@Naoises-MacBook-Pro.local>
2020-04-04 13:40:59 +0100

    Jasmine Test of FizzBuzz. Complete. Requires documentation: README.md.

--- scripts/fizzbuzz.js
+++ scripts/fizzbuzz.js
@@ -1,11 +1,13 @@
 fizzBuzz = function(number) {
-    if (typeof(number) == "number") {
+    if (typeof (number) == "number" && number <= Number.MAX_VALUE && number >= Number.MIN_VALUE) {
         if (number % 3 == 0 && number % 5 == 0) {
             return "FizzBuzz";
         } else if (number % 5 == 0) {
             return "Buzz";
         } else if (number % 3 == 0) {
             return "Fizz";
+        } else {
+            return number;
         }
 
     } else {
--- spec/fizzbuzzSpec.js
+++ spec/fizzbuzzSpec.js
@@ -1,26 +1,27 @@
 
 describe('FizzBuzz', function() {
+    // A test suite begins with a call to the global Jasmine function 'describe' with two parameters: a 'string' and a 'function'.
     beforeEach(function() {
         fibu = new fizzBuzz();
     });
     
-    describe('FizzBuzz from 1 to 100', function() {
-        
-        /* These all test positive */
+    describe(`FizzBuzz from ${Number.MIN_VALUE} to ${Number.MAX_VALUE}`, function() {
+        // Specs are defined by calling the global Jasmine function 'it'.
         it('FizzBuzz function should exist.', function() {
             expect(fizzBuzz).toBeDefined();
+            // Expectations are built with the function expect which takes a value, called the actual. 
+            // Each matcher implements a boolean comparison between the actual value and the expected value.
+            // Any matcher can evaluate to a negative assertion by chaining the call to expect with a not before calling the matcher.
         });
         
         it('Test a number that is evenly divisible by both 5 and 3.', function() {
             var result = fizzBuzz(15)
             expect(result).toBe("FizzBuzz");
-            
         });
         
         it('Test a number that is evenly divisible only by 5 and not 3.', function() {
             var result = fizzBuzz(5)
             expect(result).toBe("Buzz");
-            
         });
         
         it('Test a number that is evenly divisible only by 3 and not 5', function() {
@@ -28,24 +29,40 @@ describe('FizzBuzz', function() {
             expect(result).toBe("Fizz");
         });
         
-        /* These all test negative */
-        
-        it('--- Expected to get "undefined" (FAIL): testing FizzBuzz with a number not evenly divisible by both 3 and 5', function() {
+        it('--- Expected to get the number 13 in return, as 13 was entered and is neither evenly divisible by either 3 or 5', function() {
             var result = fizzBuzz(13)
-            expect(result).toBe(undefined);
+            expect(result).toBe(13);
         });
+        
+        it('--- Expected to get the error message "Error: you did not enter a number!"', function() {
+            var result = fizzBuzz("Abc")
+            expect(result).toBe("Error: you did not enter a number!");
+        });
+        
+        it('--- Number.MAX_VALUE is not "Fizz", nor "Buzz", nor "FizzBuzz"', function() {
+            var result = fizzBuzz(Number.MAX_VALUE)
+            expect(result).toBe(Number.MAX_VALUE);
+        });
+        
+        it('--- Number.MIN_VALUE is not "Fizz", nor "Buzz", nor "FizzBuzz"', function() {
+            var result = fizzBuzz(Number.MIN_VALUE)
+            expect(result).toBe(Number.MIN_VALUE);
+        });            
     });        
 });
 
 /*
 3.5.0Options
-finished in 0.045s
-5 specs, 0 failures, randomized with seed 54456
+finished in 0.038s
+8 specs, 0 failures, randomized with seed 12736
 FizzBuzz
-FizzBuzz from 1 to 100
-FizzBuzz function should exist.
+FizzBuzz from 5e-324 to 1.7976931348623157e+308
 Test a number that is evenly divisible only by 5 and not 3.
-Test a number that is evenly divisible by both 5 and 3.
 Test a number that is evenly divisible only by 3 and not 5
---- Expected to get "undefined" (FAIL): testing FizzBuzz with a number not evenly divisible by both 3 and 5
+--- Number.MIN_VALUE is not "Fizz", nor "Buzz", nor "FizzBuzz"
+--- Expected to get the number 13 in return, as 13 was entered and is neither evenly divisible by either 3 or 5
+--- Expected to get the error message "Error: you did not enter a number!"
+FizzBuzz function should exist.
+Test a number that is evenly divisible by both 5 and 3.
+--- Number.MAX_VALUE is not "Fizz", nor "Buzz", nor "FizzBuzz"
 */
