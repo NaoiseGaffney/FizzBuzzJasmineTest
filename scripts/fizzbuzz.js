fe2cc08c6fc199f1b7d2a3998637d5c1a279c810
Naoise Gaffney <gaff@Naoises-MacBook-Pro.local>
2020-04-04 12:58:51 +0100

    Aha, I get it now. fizzBuzz function now accepts a value as opposed to a for-loop.

--- scripts/fizzbuzz.js
+++ scripts/fizzbuzz.js
@@ -1,13 +1,9 @@
-fizzBuzz = function() {
-    for (let i = 0; i < 101; i++) {
-        if (i%3 == 0 && i%5 == 0) {
-            return "FizzBuzz";
-        } else if (i % 5 == 0) {
-            return "Buzz";
-        } else if (i % 5 == 0) {
-            return "Fizz";
-        }
+fizzBuzz = function(number) {
+    if (number%3 == 0 && number%5 == 0) {
+        return "FizzBuzz";
+    } else if (number % 5 == 0) {
+        return "Buzz";
+    } else if (number % 5 == 0) {
+        return "Fizz";
     }
-}
-
-document.getElementById("fizzBuzzOutput").innerHTML = fizzBuzz();
+};
---
+++ spec/fizzbuzzSpec.js
@@ -0,0 +1,16 @@
+
+describe('FizzBuzz', function() {
+    beforeEach(function() {
+        fibu = new fizzBuzz();
+    });
+    
+    describe('FizzBuzz from 1 to 100', function() {
+        
+        it('FizzBuzz function should exist.', function() {
+            expect(fizzBuzz).toBeDefined();
+        });
+            
+    });
+        
+});
+    
